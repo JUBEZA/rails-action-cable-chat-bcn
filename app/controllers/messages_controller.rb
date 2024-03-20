2f1028259368095bb1b51fef07de824c487994e5
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-03-20 17:19:05 -0300

    chat supuestamente funcional

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,17 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
+
     if @message.save
-      redirect_to chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(
+          partial: "messages/message",
+          locals: { message: @message }
+        )
+      )
     else
+      redirect_to chatroom_path(@chatroom)
       render "chatrooms/show", status: :unprocessable_entity
     end
   end
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,8 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
   static targets = ["messages"]
+  static values = {
+    chatroomId: Number
+  }
+
+  connect () {
+    console.log(`Connecting to the ActionCable channel with id ${this.chatroomIdValue}`)
+
+    createConsumer().subscription.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: (data) => { this.messagesTarget.insertAdjacentHTML("beforend",data) } }
+    )
+  }
+}
 
   // connect() {
   //   this.channel = createConsumer().subscription.create(
@@ -12,15 +24,16 @@ export default class extends Controller {
   //   )
   //   console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   // }
-  connect() {
-    this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      {
-        received: (data) => {
-          console.log(data);
-        }
-      }
-    );
-    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
-}
-}
+
+//   connect() {
+//     this.channel = createConsumer().subscriptions.create(
+//       { channel: "ChatroomChannel", id: this.chatroomIdValue },
+//       {
+//         received: (data) => {
+//           console.log(data);
+//         }
+//       }
+//     );
+//     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
+// }
+// }
